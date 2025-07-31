485bf1100a5455086e23c33f87c3f6ab1fdae241
Anthony M <m_tuiono@yahoo.com>
2025-07-31 09:07:02 -0700

    Update finalysis.py

--- finalysis.py
+++ finalysis.py
@@ -8,6 +8,6 @@ def fetch_data(tickers, start_date, end_date):
         stock_data = yf.download(ticker, start=start_date, end=end_date)['Close']
         data_frames.append(stock_data)
 
-        data = pd.concat(data_frames, axis=1, keys=tickers)
-        data.columns = tickers
+    data = pd.concat(data_frames, axis=1, keys=tickers)
+    data.columns = tickers
     return data
